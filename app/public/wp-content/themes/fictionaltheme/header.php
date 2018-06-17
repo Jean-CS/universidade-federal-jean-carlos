9ce6cba65c91801e2cde486671fe835992011d27
Jean Santos <jeansantosc@gmail.com>
2018-06-17 16:13:13 -0300

    charset

--- app/public/wp-content/themes/fictionaltheme/header.php
+++ app/public/wp-content/themes/fictionaltheme/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<meta charset="<?php bloginf(charset); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
