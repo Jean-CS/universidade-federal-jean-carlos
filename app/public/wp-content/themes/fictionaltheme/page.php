f99c8e25b7efef48adf1ec0920895fdec1856d02
Jean Santos <jeansantosc@gmail.com>
2018-06-17 21:18:41 -0300

    single blog page

--- app/public/wp-content/themes/fictionaltheme/page.php
+++ app/public/wp-content/themes/fictionaltheme/page.php
@@ -3,7 +3,7 @@
 while(have_posts()) {
     the_post(); ?>
 
-    <div class="page-banner">
+  <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
@@ -22,13 +22,13 @@ while(have_posts()) {
     // Only runs if the current page is a child page
     if ($theParent) { ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
-      <p>
-        <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>">
-          <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?>
-        </a> 
-        <span class="metabox__main"><?php the_title(); ?></span>
-      </p>
-    </div>
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>">
+            <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?>
+          </a> 
+          <span class="metabox__main"><?php the_title(); ?></span>
+        </p>
+      </div>
     <?php } ?>
     
     <?php 
