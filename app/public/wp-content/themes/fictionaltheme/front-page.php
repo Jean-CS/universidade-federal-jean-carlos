01d8aeda62616910dc8015f70315ae699bcd8a13
Jean Santos <jeansantosc@gmail.com>
2018-06-18 00:24:34 -0300

    use custom query to get top 2 recent posts

--- app/public/wp-content/themes/fictionaltheme/front-page.php
+++ app/public/wp-content/themes/fictionaltheme/front-page.php
@@ -15,16 +15,30 @@
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
           
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Mar</span>
-              <span class="event-summary__day">25</span>  
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-              <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
-            </div>
-          </div>
+          <?php 
+            $homepagePosts = new WP_Query(array(
+              'posts_per_page' => 2,
+              //'category_name' => 'awards', (only posts of category awards)
+              //'post_type' => 'page' (all pages)
+            ));
+
+            while($homepagePosts->have_posts()) {
+              $homepagePosts->the_post(); ?>
+
+              <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                  <span class="event-summary__month"><?php the_time('M'); ?></span>
+                  <span class="event-summary__day"><?php the_time('d'); ?></span>  
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                  <p><?php echo wp_trim_words(the_content(), 20); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                </div>
+              </div>
+
+            <?php }
+          ?>
+
           <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
               <span class="event-summary__month">Apr</span>
