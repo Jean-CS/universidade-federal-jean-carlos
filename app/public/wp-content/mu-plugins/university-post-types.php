df370a8a520905d6d7153cb7f9d7a361c3cdeb1f
Jean Santos <jeansantosc@gmail.com>
2018-06-20 00:58:01 -0300

    more label options on post_type

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -4,7 +4,11 @@ function university_post_types() {
     register_post_type('events', array(
         'public' => true, // Makes post_type public to viewers/admins of the site
         'labels' => array(
-            'name' => 'Events' // Changes the label on the wp-admin
+            'name' => 'Events', // Changes the label on the wp-admin
+            'add_new_item' => 'Add New Event', // Title header of page Add New
+            'edit_item' => 'Edit Event', // Title header of page Edit
+            'all_items' => 'All Events', // Label on the dropdown menu on the sidebar
+            'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar' //WordPress Dashicons
     ));
