026682779e874d342f25b0f7d367c444fe36a31a
Jean Santos <jeansantosc@gmail.com>
2018-06-23 23:17:05 -0300

    custom field for date in events archive

--- app/public/wp-content/themes/fictionaltheme/archive-event.php
+++ app/public/wp-content/themes/fictionaltheme/archive-event.php
@@ -14,12 +14,15 @@
 
     <?php 
       while(have_posts()) {
-        the_post(); ?>
-
+        the_post(); 
+        // Gets the custom_field 'event_date'. Created with the plugin "Advanced Custom Fields"
+        // function get_field() is part of the plugin
+        $eventDate = new DateTime(get_field('event_date'));
+        ?>
         <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month"><?php the_time('M'); ?></span>
-                <span class="event-summary__day"><?php the_time('d'); ?></span>  
+            <span class="event-summary__month"><?php echo $eventDate->format('M'); ?></span>
+                  <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>  
             </a>
             <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
--- app/public/wp-content/themes/fictionaltheme/front-page.php
+++ app/public/wp-content/themes/fictionaltheme/front-page.php
@@ -43,7 +43,7 @@
               <div class="event-summary">
                 <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
                   <span class="event-summary__month"><?php echo $eventDate->format('M'); ?></span>
-                  <span class="event-summary__day"><?php echo $eventDate->format('D'); ?></span>  
+                  <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>  
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
