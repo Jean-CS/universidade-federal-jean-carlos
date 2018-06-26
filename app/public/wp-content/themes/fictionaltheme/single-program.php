f811167944c52883768aef74162fff3c7a9650c3
Jean Santos <jeansantosc@gmail.com>
2018-06-26 00:27:50 -0300

    add: on program page, display all events related to that program

--- app/public/wp-content/themes/fictionaltheme/single-program.php
+++ app/public/wp-content/themes/fictionaltheme/single-program.php
@@ -1,32 +1,87 @@
-<?php get_header(); 
+<?php get_header();
 
-    while(have_posts()) {
-	    the_post(); ?>
+	while ( have_posts() ) {
+		the_post(); ?>
 
-	    <div class="page-banner">
-            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-            <div class="page-banner__content container container--narrow">
-                <h1 class="page-banner__title"><?php the_title(); ?></h1>
-                <div class="page-banner__intro">
-                    <p>DONT FORGET TO REPLACE ME LATER</p>
-                </div>
-            </div>  
-        </div>
+		<div class="page-banner">
+			<div class="page-banner__bg-image"
+			     style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ) ?>);"></div>
+			<div class="page-banner__content container container--narrow">
+				<h1 class="page-banner__title"><?php the_title(); ?></h1>
+				<div class="page-banner__intro">
+					<p>DONT FORGET TO REPLACE ME LATER</p>
+				</div>
+			</div>
+		</div>
 
-        <div class="container container--narrow page-section">
-            <div class="metabox metabox--position-up metabox--with-home-link">
-                <p>
-                    <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>">
-                        <i class="fa fa-home" aria-hidden="true"></i> All Programs
-                    </a> 
-                    <span class="metabox__main">
+		<div class="container container--narrow page-section">
+			<div class="metabox metabox--position-up metabox--with-home-link">
+				<p>
+					<a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link( 'program' ); ?>">
+						<i class="fa fa-home" aria-hidden="true"></i> All Programs
+					</a>
+					<span class="metabox__main">
                         <?php the_title(); ?>
                     </span>
-                </p>
-            </div>
-            <div class="generic-content"><?php the_content(); ?></div>
-        </div>
+				</p>
+			</div>
+			<div class="generic-content"><?php the_content(); ?></div>
 
-    <?php } ?>
+			<?php
+				$today          = date( 'Ymd' );
+				$homepageEvents = new WP_Query( array(
+					'posts_per_page' => 2,
+					'post_type'      => 'event',
+					'meta_key'       => 'event_date',
+					'order_by'       => 'meta_value_num', // post_date ; title ; rand
+					'order'          => 'ASC',
+					'meta_query'     => array(
+						array( // Only show posts that the 'event_date' is greater or equal to Today
+							'key'     => 'event_date',
+							'compare' => '>=',
+							'value'   => $today,
+							'type'    => 'numeric' // The type of the values we are comparing
+						),
+						array( // Gets events related programs
+							'key'     => 'related_programs',
+							'compare' => 'LIKE',
+							'value'   => '"' . get_the_ID() . '"',
+						),
+					),
+				) );
+
+				while ( $homepageEvents->have_posts() ) {
+					$homepageEvents->the_post();
+
+					// Gets the custom_field 'event_date'. Created with the plugin "Advanced Custom Fields"
+					// function get_field() is part of the plugin
+					$eventDate = new DateTime( get_field( 'event_date' ) );
+					?>
+					<div class="event-summary">
+						<a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+							<span class="event-summary__month"><?php echo $eventDate->format( 'M' ); ?></span>
+							<span class="event-summary__day"><?php echo $eventDate->format( 'd' ); ?></span>
+						</a>
+						<div class="event-summary__content">
+							<h5 class="event-summary__title headline headline--tiny"><a
+										href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+							<p>
+								<?php
+									if ( has_excerpt() ) {
+										echo get_the_excerpt();
+									} else {
+										echo wp_trim_words( get_the_content(), 18 );
+									}
+								?>
+								<a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
+							</p>
+						</div>
+					</div>
+				<?php }
+			?>
+
+		</div>
+
+	<?php } ?>
 
 <?php get_footer(); ?>
