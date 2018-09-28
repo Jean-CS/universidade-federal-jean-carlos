8424940989422b6dbb9e9617a6c3c017bae98aed
Jean Santos <jeansantosc@gmail.com>
2018-09-27 23:52:17 -0300

    add: template static view to reuse in different pages

--- app/public/wp-content/themes/fictionaltheme/front-page.php
+++ app/public/wp-content/themes/fictionaltheme/front-page.php
@@ -35,31 +35,12 @@
 
             while($homepageEvents->have_posts()) {
               $homepageEvents->the_post(); 
-
-              // Gets the custom_field 'event_date'. Created with the plugin "Advanced Custom Fields"
-              // function get_field() is part of the plugin
-              $eventDate = new DateTime(get_field('event_date'));
-              ?>
-              <div class="event-summary">
-                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                  <span class="event-summary__month"><?php echo $eventDate->format('M'); ?></span>
-                  <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>  
-                </a>
-                <div class="event-summary__content">
-                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p>
-                    <?php 
-                    if(has_excerpt()) {
-                      echo get_the_excerpt();
-                    } else {
-                      echo wp_trim_words(get_the_content(), 18);
-                    } 
-                    ?>
-                    <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
-                  </p>
-                </div>
-              </div>
-            <?php }
+              // 1st arg: file you want to include
+              // 2nd arg: name to append to file
+              // example: get_template_part('template-parts/event', 'something');
+              //          will search for file named 'template-parts/event-something';
+              get_template_part('template-parts/content', 'event');
+            }
           ?>
 
           <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
--- app/public/wp-content/themes/fictionaltheme/single-program.php
+++ app/public/wp-content/themes/fictionaltheme/single-program.php
@@ -80,43 +80,18 @@
 					),
 				) );
 			?>
-			<?php if ( $relatedEvents->have_posts() ) : ?>
-					<hr class="section-break">
-					<h2 class="headline headline--medium">Upcoming <?php get_the_title(); ?> Events</h2>
+			<?php 
+			if ( $relatedEvents->have_posts() ) { 
+			?>
+				<hr class="section-break">
+				<h2 class="headline headline--medium">Upcoming <?php get_the_title(); ?> Events</h2>
 
-					<?php while ( $relatedEvents->have_posts() ) :
-							$relatedEvents->the_post();
-
-							// Gets the custom_field 'event_date'. Created with the plugin "Advanced Custom Fields"
-							// function get_field() is part of the plugin
-							$eventDate = new DateTime( get_field( 'event_date' ) ); ?>
-
-							<div class="event-summary">
-								<a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-									<span class="event-summary__month"><?php echo $eventDate->format( 'M' ); ?></span>
-									<span class="event-summary__day"><?php echo $eventDate->format( 'd' ); ?></span>
-								</a>
-								<div class="event-summary__content">
-									<h5 class="event-summary__title headline headline--tiny">
-										<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-									</h5>
-									<p>
-										<?php
-											if ( has_excerpt() ) {
-												echo get_the_excerpt();
-											} else {
-												echo wp_trim_words( get_the_content(), 18 );
-											}
-										?>
-										<a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
-									</p>
-								</div>
-							</div>
-
-					<?php endwhile; ?>
-
-			<?php endif; ?>
-
+				<?php 
+				while ( $relatedEvents->have_posts() ) {
+					$relatedEvents->the_post();
+					get_template_part('template-parts/content', 'event');
+				}
+			}?>
 		</div>
 
 	<?php endwhile; ?>
