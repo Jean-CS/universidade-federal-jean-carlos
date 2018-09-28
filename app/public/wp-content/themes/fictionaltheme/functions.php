50841c5fdd0e865e3071b7310839a781f97b83bb
Jean Santos <jeansantosc@gmail.com>
2018-09-27 23:01:33 -0300

    refactor page banner to be used in different pages without having to copy-paste

--- app/public/wp-content/themes/fictionaltheme/functions.php
+++ app/public/wp-content/themes/fictionaltheme/functions.php
@@ -1,5 +1,45 @@
 <?php
 
+	//
+	// Partial View
+	//
+	function pageBanner($args = NULL) {
+
+		// Fallback if no title is provided as parameter to the array
+		if (!$args['title']) {
+			$args['title'] = get_the_title();
+		}
+
+		// Fallback if no subtitle is provided as parameter to the array
+		if (!$args['subtitle']) {
+			// page_banner_subtitle is a field defined in wp-admin when you create a new custom field
+			$args['subtitle'] = get_field('page_banner_subtitle');
+		}
+
+		// Fallback if no image is provided as parameter to the array
+		if (!$args['photo']) {
+			// Check if there is a custom background image for this page (custom field)
+			if (get_field('page_banner_background_image')) {
+				// If so, gets the croped version of the image
+				$args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+			} else {
+				$args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+			}
+		}
+
+		?>
+		<div class="page-banner">
+			<div class="page-banner__bg-image"
+				 style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+			<div class="page-banner__content container container--narrow">
+				<h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+				<div class="page-banner__intro">
+					<p><?php echo $args['subtitle']  ?></p>
+				</div>
+			</div>
+		</div>
+	<?php }
+
 	function university_files() {
 		university_scripts();
 		university_styles();
